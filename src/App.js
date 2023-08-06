53a9e37b843565d0fa8bdce03c31a92a18a7ba37
mzararahmed <md.zarar.ahmed@gmail.com>
2023-08-06 12:53:25 +0500

    createRoot instead of ReactDom.render for react 18

--- src/App.js
+++ src/App.js
@@ -42,7 +42,12 @@ const App = () => {
                 ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
                 : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
             }
-          ></div>
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              {/* <Navbar /> */} Navbar
+            </div>
+            <div></div>
+          </div>
         </div>
       </BrowserRouter>
     </div>
