0430fb2814eefd4345a6040df14def3ba2992979
mzararahmed <md.zarar.ahmed@gmail.com>
2023-08-06 13:07:14 +0500

    refactor, ts-ignore in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from "react-dom";
-
-import "./index.css";
-import App from "./App";
-
 import { createRoot } from 'react-dom/client';
+import React from 'react'
+import App from "./App";
+import "./index.css";
+
 const container = document.getElementById('root');
+// @ts-ignore
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App />);
