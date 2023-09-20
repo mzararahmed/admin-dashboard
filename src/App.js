2ffed4b87f2015352db52289b703c5f9c502f898
mzararahmed <zararworkahmed@gmail.com>
2023-09-20 19:57:01 +0500

    main page structure changes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { FiSettings } from "react-icons/fi";
-import { Tooltip } from "antd";
-import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
-import {
-  Area,
-  Bar,
-  Calendar,
-  ColorMapping,
-  ColorPicker,
-  Customers,
-  ECommerce,
-  Editor,
-  Employees,
-  Financial,
-  Kanban,
-  Line,
-  Orders,
-  Pie,
-  Pyramid,
-  Stacked,
-} from "./pages";
-import "./App.css";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { Tooltip } from 'antd';
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import './App.css';
+
+import { useStateContext } from './contexts/ContextProvider';
+
 const App = () => {
-  const activeMenu = true;
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
             <Tooltip
               title="Settings"
-            // position="Top"
             >
               <button
                 type="button"
-                // onClick={() => setThemeSettings(true)}
-                style={{ background: "blue", borderRadius: "50%" }}
-                // style={{ background: currentColor, borderRadius: '50%' }}
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: '50%' }}
                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
               </button>
+
             </Tooltip>
           </div>
           {activeMenu ? (
@@ -56,18 +52,20 @@ const App = () => {
           <div
             className={
               activeMenu
-                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
-                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
             }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
             <div>
+              {themeSettings && (<ThemeSettings />)}
+
               <Routes>
                 {/* dashboard  */}
-                <Route path="/" element={(<ECommerce />)} />
-                <Route path="/ecommerce" element={(<ECommerce />)} />
+                <Route path="/" element={(<Ecommerce />)} />
+                <Route path="/ecommerce" element={(<Ecommerce />)} />
 
                 {/* pages  */}
                 <Route path="/orders" element={<Orders />} />
@@ -89,8 +87,10 @@ const App = () => {
                 <Route path="/color-mapping" element={<ColorMapping />} />
                 <Route path="/pyramid" element={<Pyramid />} />
                 <Route path="/stacked" element={<Stacked />} />
+
               </Routes>
             </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
